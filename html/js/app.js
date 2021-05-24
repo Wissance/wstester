814669fe728b3fd480b11006e912f246b18cdf21
Ushakov Michael <um.nix.user@gmail.com>
2021-05-24 10:33:34 +0500

    js moved to separate source

---
+++ html/js/app.js
@@ -0,0 +1,69 @@
+    var socketAddress = "";
+    var socket = null;
+
+    function preparePage() {
+        console.log("Page preparing ...");
+        var date = new Date();
+        $("#current_year").text(date.getFullYear())
+    }
+
+    function openWsServer() {
+        var address = $("#ws_address").val();
+        socketAddress = buildServerAddress(address, false);  // todo: umv: add checkbox here
+        console.log("Websocket open server: " + address);
+        socket = new WebSocket(socketAddress)
+                // todo: analyze was socket conn establish was successful or not
+                // todo: UMV: log to logs textarea
+        socket.onmessage = function (event) {
+            console.log("Got message from server");
+            // todo: UMV: log to logs textarea
+         }
+    }
+
+    function closeWsServer() {
+        console.log("Websocket close server");
+        if (socket != null) {
+            socket.close();
+            socket = null;
+             // todo: UMV: log to logs textarea
+        }
+    }
+
+    function sendDataToWsServer() {
+        if (socket != null) {
+            console.log("Websocket send data");
+            var message = $("#ws_payload").val();
+            if (message.startsWith("{")) {
+                message = JSON.stringify(message);
+            }
+            socket.send(message);
+            // todo: UMV: add log to logs textarea here
+        }
+    }
+
+    function buildServerAddress(address, secure) {
+                if (!(address.startsWith("ws://") || address.startsWith("wss://")))
+                    return secure ? "wss://" + address : "ws://" + address;
+                return address;
+    }
+
+    function logWsEvent(messageType, messageData) {
+        var logMsg = createLogMessage(messageType, messageData);
+        $("#ws_logs").append(logMsg);
+    }
+
+    function createLogMessage(messageType, messageData) {
+        var dateStr = getFormattedDate();
+        var logStr = dateStr + ": " + messageType;
+        if (messageData != null && messageData.length > 0) {
+            logStr = logStr + " : " + messageData
+        }
+        return messageData;
+    }
+
+    function getFormattedDate() {
+        var date = new Date();
+        var str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+        return str;
+    }
